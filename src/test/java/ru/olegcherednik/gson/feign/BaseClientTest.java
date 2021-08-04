8cf773349758b36aaf30ceb4a3275107ea1f2953
Oleg Cherednik <oleg.cherednik@gmail.com>
2021-08-04 23:26:22 +0300

    build.gradle

--- src/test/java/ru/olegcherednik/gson/feign/BaseClientTest.java
+++ src/test/java/ru/olegcherednik/gson/feign/BaseClientTest.java
@@ -1,7 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package ru.olegcherednik.gson.feign;
 
 import feign.Feign;
-import feign.Logger;
+import feign.RequestInterceptor;
 import feign.okhttp.OkHttpClient;
 import feign.spring.SpringContract;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +27,6 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import ru.olegcherednik.gson.feign.app.server.SpringBootApp;
 import ru.olegcherednik.gson.utils.GsonDecorator;
-import ru.olegcherednik.gson.utils.GsonUtilsHelper;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringBootApp.class)
 public abstract class BaseClientTest extends AbstractTestNGSpringContextTests {
@@ -17,22 +34,14 @@ public abstract class BaseClientTest extends AbstractTestNGSpringContextTests {
     @LocalServerPort
     protected int randomServerPort;
 
-    protected <T> T buildClient(Class<T> clientClass) {
-        GsonDecorator gson = GsonUtilsHelper.createGsonDecorator();
-//        ObjectMapper objectMapper = new ObjectMapper()
-//                .registerModule(new Jdk8Module())
-//                .registerModule(new JavaTimeModule());
-
+    protected <T> T buildClient(Class<T> cls, GsonDecorator gson, RequestInterceptor requestInterceptor) {
         return Feign.builder()
                     .contract(new SpringContract())
                     .client(new OkHttpClient())
                     .encoder(new GsonEncoder(gson))
                     .decoder(new GsonDecoder(gson))
-//                    .encoder(new JacksonEncoder(objectMapper))
-//                    .decoder(new JacksonDecoder(objectMapper))
-//                    .logger(new Slf4jLogger(FileSetClient.class))
-                    .logLevel(Logger.Level.FULL)
-                    .target(clientClass, String.format("http://localhost:%d", randomServerPort));
+                    .requestInterceptor(requestInterceptor)
+                    .target(cls, String.format("http://localhost:%d", randomServerPort));
     }
 
 }
--- src/test/java/ru/olegcherednik/gson/feign/BookClientTest.java
+++ src/test/java/ru/olegcherednik/gson/feign/BookClientTest.java
@@ -1,43 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package ru.olegcherednik.gson.feign;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Import;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.olegcherednik.gson.feign.app.client.BookClient;
 import ru.olegcherednik.gson.feign.app.dto.Book;
 import ru.olegcherednik.gson.feign.app.server.BookController;
+import ru.olegcherednik.gson.utils.GsonDecorator;
+import ru.olegcherednik.gson.utils.GsonUtilsHelper;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Test
 @Import(BookController.class)
 public class BookClientTest extends BaseClientTest {
 
-    private BookClient client;
+    public void shouldUseGivenGsonDecoratorWhenWorkWithJson() {
+        for (GsonDecorator gson : Arrays.asList(GsonUtilsHelper.createGsonDecorator(),
+                GsonUtilsHelper.createPrettyPrintGsonDecorator())) {
+            LocalRequestInterceptor requestInterceptor = new LocalRequestInterceptor();
 
-    @BeforeMethod
-    public void setUp() {
-        client = buildClient(BookClient.class);
+            BookClient client = buildClient(BookClient.class, gson, requestInterceptor);
+            Book book = new Book();
+            book.setTitle("title");
+            book.setAuthor("author");
+            Book actual = client.createBook(book);
+            assertThat(actual).isNotNull();
+            assertThat(actual.getTitle()).isEqualTo(book.getTitle());
+            assertThat(actual.getAuthor()).isEqualTo(book.getAuthor());                                                                 assertThat(actual.getResponse()).isEqualTo(BookController.class.getSimpleName());
+            assertThat(requestInterceptor.body).isEqualTo(gson.writeValue(book));
+        }
     }
 
-    public void foo() {
-        Book book = new Book();
-        book.setTitle("title");
-        book.setAuthor("author");
+    private static final class LocalRequestInterceptor implements RequestInterceptor {
 
-        Book actual = client.createBook(book);
+        private String body;
 
-        int a = 0;
-        a++;
+        @Override
+        public void apply(RequestTemplate template) {
+            body = new String(template.body());
+        }
+
     }
-
-//    public void bar() {
-//        Book book = new Book();
-//        book.setTitle("title");
-//        book.setAuthor("author");
-//
-//        Book actual = client.createBook(book);
-//
-//        int a = 0;
-//        a++;
-//    }
 
 }
